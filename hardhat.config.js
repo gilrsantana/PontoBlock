3646a6d05dbc2b3c998dc3b5f439cad0e9c87a2d
gilrsantana <g.r.santana@hotmail.com>
2023-02-13 05:10:08 -0300

    Configurinh hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,32 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+/** 
+ * @type import('hardhat/config').HardhatUserConfig 
+ */
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
+const { ALCHEMY_API_URL, ACCOUNT_PRIVATE_KEY, POLYGON_API_KEY } = process.env;
+
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "mumbai",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: ALCHEMY_API_URL,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: {
+      mumbai: POLYGON_API_KEY
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
 };
